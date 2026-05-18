182fe7769301733367227fee781db256ac1762e5
ROG STRIX <ROG STRIX@DESKTOP-NDTL53S>
2026-05-18 12:58:27 +0700

    fix DTOs.cs

--- Website/Dtos/DTOs.cs
+++ Website/Dtos/DTOs.cs
@@ -29,7 +29,6 @@
             public string Name { get; set; } = string.Empty;
             public string? Description { get; set; }
             public decimal Price { get; set; }
-            public int Stock { get; set; }
             public IFormFile? Image { get; set; }
             public int CategoryId { get; set; }
         }
@@ -39,7 +38,6 @@
             public string Name { get; set; } = string.Empty;
             public string? Description { get; set; }
             public decimal Price { get; set; }
-            public int Stock { get; set; }
             public IFormFile? Image { get; set; }
             public int CategoryId { get; set; }
         }
@@ -50,7 +48,6 @@
             public string Name { get; set; } = string.Empty;
             public string? Description { get; set; }
             public decimal Price { get; set; }
-            public int Stock { get; set; }
             public string? ImagePath { get; set; }
             public string? ImageUrl { get; set; }
             public int CategoryId { get; set; }
